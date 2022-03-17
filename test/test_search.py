865472ecc4b4c1c82e547fa1d0fe0c2921f8c14b
Kyle Bittinger <kylebittinger@gmail.com>
2022-03-17 14:07:57 -0400

    Create tests for RefseqAssembly

--- test/test_search.py
+++ test/test_search.py
@@ -1,19 +1,6 @@
 import collections
-import os
 
 from stackebrandtcurves.ssu import Refseq16SDatabase
-from stackebrandtcurves.assembly import RefseqAssembly
-
-DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
-ASSEMBLY_SUMMARY_FP = os.path.join(DATA_DIR, "muribaculum_assembly_summary.txt")
-
-def test_load():
-    assemblies = RefseqAssembly.load(ASSEMBLY_SUMMARY_FP)
-    a = assemblies["GCF_001688845.2"]
-    assert a.accession == "GCF_001688845.2"
-    assert a.ftp_path == (
-        "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/001/688/845/"
-        "GCF_001688845.2_ASM168884v2")
 
 MockAssembly = collections.namedtuple("Assembly", ["accession", "ssu_seqs"])
 
