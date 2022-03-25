4dd8ecacf08fdb03739a095e617db988e22ca0a6
Kyle Bittinger <kylebittinger@gmail.com>
2022-03-25 09:46:33 -0400

    Clean up RefSeq class, remove assembly interface in favor of accessions

--- src/stackebrandtcurves/refseq.py
+++ src/stackebrandtcurves/refseq.py
@@ -15,16 +15,8 @@ class RefSeq:
         self.data_dir = data_dir
         self.assemblies = {}
         self.seqs = {}
-        self.assembly_seqs = {}
         self.accession_seqids = {}
-        self.seqid_assemblies = {}
         self.seqid_accessions = {}
-
-    def get_query_seqid(self, accession):
-        seqids = self.accession_seqids.get(accession)
-        if seqids:
-            return seqids[0]
-        return None
 
     @property
     def assembly_summary_fp(self):
@@ -44,27 +36,23 @@ class RefSeq:
         return self.assemblies
 
     def load_seqs(self):
-        for assembly in self.assemblies.values():
-            seqs = list(self.get_16S_seqs(assembly))
-            self.assembly_seqs[assembly.accession] = seqs
-            self.accession_seqids[assembly.accession] = [seqid for seqid, seq in seqs]
+        for accession in self.assemblies.keys():
+            seqs = list(self.get_16S_seqs(accession))
+            self.accession_seqids[accession] = [seqid for seqid, seq in seqs]
             for seqid, seq in seqs:
                 self.seqs[seqid] = seq
-                self.seqid_assemblies[seqid] = assembly
-                self.seqid_accessions[seqid] = assembly.accession
+                self.seqid_accessions[seqid] = accession
 
     def save_seqs(self):
         with open(self.ssu_fasta_fp, "w") as f:
-            for assembly in self.assemblies.values():
-                for seqid, seq in self.assembly_seqs[assembly.accession]:
-                    f.write(">{0}\n{1}\n".format(seqid, seq))
+            for seqid, seq in self.seqs.items():
+                f.write(">{0}\n{1}\n".format(seqid, seq))
 
     def save_filtered_seqs(self, fp, seen):
         with open(fp, "w") as f:
-            for assembly in self.assemblies.values():
-                for seqid, seq in self.assembly_seqs[assembly.accession]:
-                    if seqid not in seen:
-                        f.write(">{0}\n{1}\n".format(seqid, seq))
+            for seqid, seq in self.seqs.items():
+                if seqid not in seen:
+                    f.write(">{0}\n{1}\n".format(seqid, seq))
 
     @property
     def genome_dir(self):
@@ -76,9 +64,6 @@ class RefSeq:
 
     def collect_genome(self, accession):
         assembly = self.assemblies[accession]
-        return self.download_genome(assembly)
-
-    def download_genome(self, assembly):
         genome_fp = self.genome_fp(assembly)
         if os.path.exists(genome_fp):
             return genome_fp
@@ -96,7 +81,8 @@ class RefSeq:
         rna_filename = "{0}_rna_from_genomic.fna".format(assembly.basename)
         return os.path.join(self.rna_dir, rna_filename)
 
-    def download_rna(self, assembly):
+    def download_rna(self, accession):
+        assembly = self.assemblies[accession]
         rna_fp = self.rna_fp(assembly)
         if os.path.exists(rna_fp):
             return rna_fp
@@ -107,8 +93,8 @@ class RefSeq:
         subprocess.check_call(["gunzip", "-q", rna_fp + ".gz"])
         return rna_fp
 
-    def get_16S_seqs(self, assembly):
-        rna_fp = self.download_rna(assembly)
+    def get_16S_seqs(self, accession):
+        rna_fp = self.download_rna(accession)
         with open(rna_fp, "rt") as f:
             for desc, seq in parse_fasta(f):
                 if is_full_length_16S(desc):
